6c3802342f4fe3d858a6235120ca41cd6868adcf
0xf00I <114118401+0xf00I@users.noreply.github.com>
2024-01-29 23:11:31 +0000

    !

--- src/anti-disassembler.h
+++
@@ -1,8 +0,0 @@
-/* #ifndef ANTI_DISASSEMBLER_H
-#define ANTI_DISASSEMBLER_H
-
-#endif
-
-#define JUNK \
-    __asm__ volatile("push     %eax      \n"\
-                     "pop      %eax      \n"); */
